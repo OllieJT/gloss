7df4f2c34b90ebc2f7900482446cc390f89dcf7e
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2024-12-23 20:06:29 +0000

    Adds define utils

---
+++ src/lib/define.ts
@@ -0,0 +1,5 @@
+import type { GlossComponentData, GlossConfigData } from '$lib/types.js';
+
+export const defineGlossConfig = (config: GlossConfigData) => config;
+
+export const defineGlossComponent = (config: GlossComponentData) => config;
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,3 @@
 export * from './config.svelte.js';
+export * from './define.js';
 export * from './types.js';
